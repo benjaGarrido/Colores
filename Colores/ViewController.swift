6c290f151daa8099dcacf73a22ac15798c9cd04d
Benjamín Garrido Barreiro <benjamin.garrido@gmail.com>
2016-12-12 21:58:33 +0100

    bgarrido - finishing transform methods

--- Colores/ViewController.swift
+++ Colores/ViewController.swift
@@ -87,6 +87,63 @@ class ViewController: UIViewController {
         super.touchesBegan(touches, with: event)
     }
     
+    // Momento en el que levantamos el dedo
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            // Comprobamos que donde estamos tocando es el knob
+            if touch.view == imgKnob {
+                deltaAngle = nil
+                startTransform = nil
+            }
+        }
+        super.touchesEnded(touches, with: event)
+    }
+    
+    // Controlamos el movimiento del dedo (mientras lo arrastramos)
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first, let deltaAngle = deltaAngle, let startTransform = startTransform, touch.view == #imageLiteral(resourceName: "img_knob") {
+            // Cogemos la posición en el knob de nuestro dedo
+            let position = touch.location(in: imgKnob)
+            // Calculamos la distancia desde el centro a donde tenemos el dedo
+            let dist = calculateDistanceFromCenter(position)
+            // Comprobamos si estamos dentro del knob o fuera
+            if touchIsInKnobWithDistance(distance: dist) {
+                // Calculamos el ángulo según arrastramos
+                let center = CGPoint(x: imgKnob.bounds.size.width/2, y: imgKnob.bounds.size.height/2)
+                let deltaX = position.x - center.x
+                let deltaY = position.y - center.y
+                let angle = atan2(deltaY, deltaX)
+                
+                // Calculamos la distancia con el anterior
+                let angleDif = deltaAngle - angle
+                // Rotamos la imágen en base al ángulo de diferencia
+                let newTransform = startTransform.rotated(by: -angleDif)
+                // Tenemos que guardar el ultimo lugar donde esta el punto del knob
+                let lastSetPointAngle = setPointAngle
+                // Comprobamos que no sobrepasamos los límites establecidos (de 30 grados)
+                // Al anterior le sumamos lo que nos hemos movido
+                setPointAngle = setPointAngle + Double(angleDif)
+                if setPointAngle >= minAngle && setPointAngle <= maxAngle {
+                    // En este caso estamos dentro de los límites y por lo tanto cambiamos el color
+                    // Y aplicamos la transformada
+                    view.backgroundColor = UIColor(hue: colorValueFromAngle(angle: setPointAngle), saturation: 0.75, brightness: 0.75, alpha: 1.0)
+                    imgKnob.transform =  newTransform
+                    self.startTransform = newTransform
+                } else {
+                    // Si no está en el límite (se pasa) lo dejamos en el límite
+                    setPointAngle = lastSetPointAngle
+                }
+            }
+            
+        }
+        super.touchesMoved(touches, with: event)
+    }
+    
+    private func colorValueFromAngle(angle: Double)-> CGFloat {
+        let hueValue = (angle - minAngle) * (360 / maxAngle - minAngle)
+        return CGFloat(hueValue / 360)
+    }
+    
     // Esto es el teorema de pitágoras
     private func calculateDistanceFromCenter(_ point: CGPoint)-> CGFloat {
         let center = CGPoint(x: imgKnob.bounds.size.width/2, y: imgKnob.bounds.size.height/2)
