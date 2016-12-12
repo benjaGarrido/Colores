0767041f71b9bf12bed70056cdf77246d63731f7
Benjamín Garrido Barreiro <benjamin.garrido@gmail.com>
2016-12-12 21:14:30 +0100

    bgarrido - starting with the variables declaration and functions

--- Colores/ViewController.swift
+++ Colores/ViewController.swift
@@ -9,17 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var btnSwitch: UIButton!
+    @IBOutlet weak var imgKnobBase: UIImageView!
+    @IBOutlet weak var imgKnob: UIImageView!
 
+    private var deltaAngle: CGFloat?
+    private var startTransform: CGAffineTransform?
+    
+    // Se corresponde con PI/2 que es la vertical
+    private var setPointAngle = M_PI_2
+    
+    // Establecemos los límites tomando como referencia un ángulo de 30 grados (PI/6 = 30 grados)
+    private var maxAngle = 7 * M_PI / 6
+    private var minAngle = 0 - (M_PI/6)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imgKnob.isHidden = true
+        imgKnobBase.isHidden = true
+        imgKnob.isUserInteractionEnabled = true
+    }
+    
+    @IBAction func btnSwitchPressed(_ sender: Any) {
+        btnSwitch.isSelected = !btnSwitch.isSelected
+        if btnSwitch.isSelected {
+            view.backgroundColor = UIColor(hue:0.5,saturation:0.5,brightness:0.75,alpha:1.0)
+            imgKnob.isHidden = false
+            imgKnobBase.isHidden = false
+        } else {
+            view.backgroundColor = UIColor(hue:0.5,saturation:0,brightness:0.2,alpha:1.0)
+            imgKnob.isHidden = true
+            imgKnobBase.isHidden = true
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        btnSwitch.setImage(#imageLiteral(resourceName: "img_switch_off"), for: .normal)
+        btnSwitch.setImage(#imageLiteral(resourceName: "img_switch_on"), for: .selected)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override var prefersStatusBarHidden: Bool {
+        return true
     }
-
-
 }
 
